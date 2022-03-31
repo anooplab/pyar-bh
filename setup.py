a12558862ab7e42cfcc4885c536e2fd1872c3b8d
Anoop A <anoop@chem.iitkgp.ac.in>
2022-03-31 08:31:27 +0530

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='pyar-de',
+    name='pyar-bh',
     version='0.1',
-    packages=['pyar_de'],
-    scripts=['scripts/pyar-de'],
-    url='https://github.com/anooplab/pyar-de',
+    packages=['pyar_bh'],
+    scripts=['scripts/pyar-bh'],
+    url='https://github.com/anooplab/pyar-bh',
     license='GPL',
     author='anoop',
     author_email='anoop@chem.iitkgp.ac.in',
-    description='Global optimization of molecules using Differential Evolution '
+    description='Global optimization of molecules using Basinhopping '
                 'in SciPy. Just a blackbox implementation.'
 )
